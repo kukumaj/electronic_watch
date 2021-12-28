33ee869398035cb0f2dfca773a3be5ca3fee91f2
Ryszard <zakumaj@gmail.com>
2021-12-28 18:17:19 +0100

    correction second version of electronic watch

--- src/electronic_watch.java
+++ src/electronic_watch.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 public class electronic_watch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give value of seconds since midnight: ");
-        int secondsSinceMidnight = scanner.nextInt();
+        int seconds = scanner.nextInt();
 
         int secondsInDay = 60 * 60 * 24;
         int secondsInHour = 60 * 60;
-        int hours = (secondsSinceMidnight % secondsInDay) / secondsInHour;
-        int minutes = (secondsSinceMidnight % secondsInHour) / 60;
-        int seconds = secondsSinceMidnight % 60;
+        int hours = (seconds % secondsInDay) / secondsInHour;
+        int minutes = (seconds % secondsInHour) / 60;
+        int seconds2 = seconds % 60;
 
         int minutesFirstDigit = minutes / 10;
         int minutesLastDigit = minutes % 10;
 
-        int secondsFirstDigit = seconds / 10;
-        int secondsLastDigit = seconds % 10;
+        int secondsFirstDigit = seconds2 / 10;
+        int secondsLastDigit = seconds2 % 10;
 
         System.out.println(hours + ":" + minutesFirstDigit + minutesLastDigit + ":" + secondsFirstDigit + secondsLastDigit);
     }
